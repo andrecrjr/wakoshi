20f10616f97df978cae3e205e05b1634d30b1ce7
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-03-18 23:01:22 -0300

    - update with package

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,53 +2,19 @@ import { RootUserProfile, UserProfile } from "@/app/types/wakatimeAPI";
 import { tokenConverter } from "@/utils";
 import axios from "axios";
 import { NextAuthOptions, User } from "next-auth";
+import WakatimeProvider from "next-auth-wakatime";
 import NextAuth from "next-auth/next"
 
 
 export const authOptions:NextAuthOptions = NextAuth({
-  providers: [
-    {
-      id: "wakatime",
-      name: "Wakatime",
-      type: "oauth",
-      clientId: process.env.WAKATIME_CLIENT_ID,
-      clientSecret: process.env.WAKATIME_CLIENT_SECRET,
-      version: "2.0",
-      token: {url:"https://wakatime.com/oauth/token",
-      async request(context) {
-        const response = await axios.post("https://wakatime.com/oauth/token",
-              {...context.params, redirect_uri:context.provider.callbackUrl},
-            );
-            const tokenWaka = tokenConverter(response.data)
-        return {tokens:tokenWaka}
-      }, 
-      params:{
-        client_id:process.env.WAKATIME_CLIENT_ID,
-        client_secret:process.env.WAKATIME_CLIENT_SECRET,
-        grant_type:"authorization_code"
-      }},
-      authorization: {url:"https://wakatime.com/oauth/authorize?response_type=code", params:
-      {
-        scope:"email,read_stats,read_summaries",
-        "grant_type":"authorization_code"
-      }
-    },
-    userinfo: {
-      async request(context){
-        const response = await axios.get("https://wakatime.com/api/v1/users/current", {
-          headers: {Authorization: `Bearer ${context.tokens.access_token}`}
-        })
-        return response.data
-      }
-    },
-    idToken:false,
-    profile(profile) {
-      const data: UserProfile = profile.data
-      return data
-    },
-    accessTokenUrl: "https://wakatime.com/oauth/token",
-    },
-
+  providers: [WakatimeProvider({
+        clientId:process.env.WAKATIME_CLIENT_ID!,
+        clientSecret:process.env.WAKATIME_CLIENT_SECRET!,
+        authorization: {
+        params: {
+          scope: "email,read_stats,read_summaries", // DON'T FORGET this is Required scopes in the code to work
+        }
+      }})
     ], 
     secret: process.env.NEXTAUTH_SECRET,
     debug:process.env.NODE_ENV !== "production",
@@ -60,7 +26,6 @@ export const authOptions:NextAuthOptions = NextAuth({
       async jwt({ token, account, user }) {
         // add user data only when updates the session (click in sign in on front-end)
         user && (token.user = user);
-        console.log(user)
        return Promise.resolve(token);
      },
       async session({session, token, user}) {
